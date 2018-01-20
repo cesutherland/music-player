5b15c021ee3d308452d16fb4abbbfaff86e60c37
Carl Sutherland <carl@humblesoftware.com>
2018-01-19 20:08:29 -0500

    Fix key sizes.

--- migrations/20180113142541_init.js
+++ migrations/20180113142541_init.js
@@ -5,17 +5,17 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('oauth', function (table) {
       table.increments('id').primary().index();
       table.integer('user_id').index();
-      table.string('session', 255).index();
+      table.string('session', 1024).index();
       table.string('key', 255);
-      table.string('access_token', 255);
-      table.string('refresh_token', 255);
+      table.string('access_token', 1024);
+      table.string('refresh_token', 1024);
       table.integer('expires');
       table.boolean('connected');
     }),
 
     knex.schema.createTable('tracks', function (table) {
       table.increments('id').primary().index();
-      table.string('foreign_id', 255).unique();
+      table.string('foreign_id', 1024).unique();
       table.json('track');
     }),
 
