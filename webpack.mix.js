bd7ed5c37a4c6f47594bba3b23ea23a4d28d7703
Carl Sutherland <carl@humblesoftware.com>
2019-11-09 14:38:09 -0500

    Add mix build.

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+
+// Override font output dir for webpack file-loader?
+mix.config.fileLoaderDirs.fonts = 'public/fonts';
+
+mix
+  .react('client/index.js', 'public/app.js')
+  .less('client/index.less', 'public/app.css')
+  .copy('client/index.html', 'public/')
+  .sourceMaps()
+  .browserSync({
+    port: 8080,
+    proxy: 'localhost:3000'
+  });
