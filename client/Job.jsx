418ae97e506f0ed7a2a64021dd7b4764d83724e1
Carl Sutherland <carl@humblesoftware.com>
2020-04-03 13:33:56 -0400

    Wire up albums progress.

--- client/Job.jsx
+++ client/Job.jsx
@@ -6,19 +6,40 @@ import React from 'react';
 //   albums: {total: 505, progress: 195},
 // }
 
-export default (job, {tracks, playlists, albums} = {}) =>  {
+const Job = (job, jobProgress) => {
 
-  const importing         = !job || job.finished;
-  const tracksProgress    = tracks && tracks.progress / tracks.total;
-  const playlistsProgress = playlists && playlists.progress / playlists.total;
-  const albumsProgress    = albums && albums.progress / albums.total;
+  const importing = !job || job.finished;
+  const types = ['tracks', 'albums', 'playlists'];
+  const hasProgress = types.reduce((hasProgress, type) => hasProgress || jobProgress[type].progress);
+
+  const Progress = (type) => {
+    const data = jobProgress[type];
+    const progress = data && formatPercent(data.progress/data.total) || 0;
+    const active = progress != 0 && progress != 100;
+    return (
+      <div className={active ? 'row active' : 'row'}>
+        <div className="col-xs-6 text-right">{type}:</div>
+        <div className="col-xs-6 text-left">{progress}%</div>
+      </div>
+    );
+  }
 
   return (
     <div className="splash job-progress">
       {!job && <div>loading...</div>}
-      {job && !tracksProgress && !playlistsProgress && !albumsProgress && <div>preparing import</div>}
-      {job && tracksProgress ? <div>importing tracks:    {(Math.round(tracksProgress * 100))}%</div> : ''}
-      {playlistsProgress ? <div>importing playlists: {(Math.round(playlistsProgress * 100))}%</div> : ''}
+      {job && !hasProgress && <div>preparing import</div>}
+      {hasProgress && (
+        <div>
+          <div className="row">
+            <div className="col-xs-6 text-right">importing...</div>
+          </div>
+          {types.map(type => Progress(type))}
+        </div>
+      )}
     </div>
   );
 };
+
+const formatPercent = (progress) => Math.round(progress * 100);
+
+export default Job; 
